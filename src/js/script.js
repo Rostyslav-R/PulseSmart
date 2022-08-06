73a0868bd4e81e9bb84457d185347397a9f21ec3
Rostyslav Real <real.rstslv@gmail.com>
2022-08-06 13:07:46 +0300

    Создал блок с каталогом товаров и первую обертку для товара

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,14 @@ $(document).ready(function(){
         adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icon/catalog/chevron_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icon/catalog/chevron_right.png"></button>',
-        autoplay: true,
-        autoplaySpeed: 2000
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
